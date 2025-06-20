d61d3ee82c45611deaa9311890ae7afafe852a3c
Julien Jamme <julien.jamme@protonmail.com>
2025-06-20 18:50:38 +0200

    fix chemin data

--- pratique/R/fun_import_data.R
+++ pratique/R/fun_import_data.R
@@ -2,7 +2,7 @@
 # La construction des données est réalisée par pratique/R/prepa_data.R
 
 import_lfs <- function(){
-  data.table::fread("data/lfs_2023.csv") %>%
+  data.table::fread("../../data/lfs_2023.csv") %>%
     mutate( across( c(-HHID, -AGE, -IS_CHOM), as.factor))
 }
 
